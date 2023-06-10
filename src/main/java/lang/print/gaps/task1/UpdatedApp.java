7439ae5e18b29735a4ef4b8b2b867837821a53f5
khatia ormotsadze <khatiaormotsadze99@gmail.com>
2023-06-10 15:08:38 +0400

    git commit -m"Modify UpdatedApp.java"

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -4,6 +4,6 @@ public class UpdatedApp {
     public static void main(String[] args) {
 
         System.out.println("I'm updated\n" +
-                "can be compiled. And can write to console\"");
+                "can be compiled. And can write to console");
     }
 }
